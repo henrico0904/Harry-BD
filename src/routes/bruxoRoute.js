991801cc05dabfee3ccf467f467a634631c5f000
Henrico <henrico.ferreira@aluno.senai.br>
2025-10-27 14:48:27 -0300

    feat: adiciona o verbo 'Update'

--- src/routes/bruxoRoute.js
+++ src/routes/bruxoRoute.js
@@ -11,6 +11,8 @@ router.get("/:id", BruxoController.listarUm)
 router.post("/", BruxoController.criar)
 //rota delete
 router.delete("/:id", BruxoController.apagar)
+//rota update
+router.put("/:id", BruxoController.atualizar)
 
 
 
